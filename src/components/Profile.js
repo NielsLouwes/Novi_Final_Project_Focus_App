7d83c448203d73bf231fa1f70b18dfb9f63ecff7
Niels Louwes <niels.louwes@and.digital>
2022-03-16 14:21:23 +0100

    added basic info profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -11,15 +11,17 @@ export default function Profile() {
   const history = useHistory();
 
   async function handleSubmit(e) {}
-
+ console.log(currentUser);
   return (
     <div className="Profile">
       {error && <h2>{error}</h2>}
       <form onSubmit={handleSubmit} className="ProfileMain">
         <h2 className="profile-title">My profile </h2>
+        <p>First Name: </p>
+        <p>Last Name: </p>   
         {currentUser ? (
-          <label className="register__label" htmlFor="email">
-            <strong>Email:</strong> {currentUser.email}
+          <label  className="register__label" htmlFor="email">
+            <strong className="register__label" >Email:</strong> {currentUser.email}
           </label>
         ) : (
           <Redirect to="/signin"> </Redirect>
