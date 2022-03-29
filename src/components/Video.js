b27bed804aed73d7144a67763d9433ae8c5715fd
Niels Louwes <niels.louwes@and.digital>
2022-03-29 21:43:33 +0200

    fixed destructured props

--- src/components/Video.js
+++ src/components/Video.js
@@ -2,19 +2,19 @@ import React  from "react";
 import "./Video.css"
 import YoutubeEmbed from "./YoutubeEmbed";
 
-export default function VideoCard(props) {
+export default function VideoCard({id, title, channel, views}) {
 
     const BASE_EMBED_URL = 'https://www.youtube.com/embed/';
-    const embedURL = `${BASE_EMBED_URL} ${props.id}`;
+    const embedURL = `${BASE_EMBED_URL} ${id}`;
 
     return (
         <div className="videoCard">
             <div className="videoCard__info">
-                <YoutubeEmbed  className="video-player" src={embedURL}  embedId={props.id} />
+                <YoutubeEmbed  className="video-player" src={embedURL}  embedId={id} />
                 <div className="videoCard__description">
-                    <h4 >{props.title}</h4>
-                    <p>{props.channel}</p>
-                    <p>{props.views}</p>
+                    <h4 >{title}</h4>
+                    <p>{channel}</p>
+                    <p>{views}</p>
                 </div>
             </div>
         </div>
