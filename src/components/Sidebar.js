e33acf0b169d64aae8700be867819a2ea29e21d7
Niels Louwes <niels.louwes@and.digital>
2022-04-04 21:12:45 +0200

    made rough pomo timer

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./sidebar.css";
 
@@ -16,38 +16,97 @@ const titleAndGenres = {
 };
 
 export default function Sidebar() {
+  const [time, setTime] = useState(0);
+
   return (
     <div className="sidebar-container">
       <div className="sidebarRow">
         <h2 className="sidebarRow__genre">{titleAndGenres.title.one}</h2>
-        <Link className="sidebarRow__genre" to="/choose-music/lofi-hiphop">{lofiGenres[0]}</Link>
-        <Link className="sidebarRow__genre" to="/choose-music/lofi-chill">Chill</Link>
-        <Link className="sidebarRow__genre" to="/choose-music/lofi-piano">Piano</Link>
+        <Link className="sidebarRow__genre" to="/choose-music/lofi-hiphop">
+          {lofiGenres[0]}
+        </Link>
+        <Link className="sidebarRow__genre" to="/choose-music/lofi-chill">
+          Chill
+        </Link>
+        <Link className="sidebarRow__genre" to="/choose-music/lofi-piano">
+          Piano
+        </Link>
       </div>
 
       <div className="sidebarRow">
         <h2 className="sidebarRow__genre">{titleAndGenres.title.two}</h2>
-        <Link className="sidebarRow__genre" to="/choose-music-classical-music-classical">Classical</Link>
-        <Link className="sidebarRow__genre" to="/choose-music-classical-music-neo-classical">
+        <Link
+          className="sidebarRow__genre"
+          to="/choose-music-classical-music-classical"
+        >
+          Classical
+        </Link>
+        <Link
+          className="sidebarRow__genre"
+          to="/choose-music-classical-music-neo-classical"
+        >
           Neo Classical
         </Link>
-        <Link className="sidebarRow__genre" to="/choose-music-classical-music-dark-academia">
+        <Link
+          className="sidebarRow__genre"
+          to="/choose-music-classical-music-dark-academia"
+        >
           Dark Academia
         </Link>
       </div>
 
       <div className="sidebarRow">
         <h2 className="sidebarRow__genre">{titleAndGenres.title.three}</h2>
-        <Link className="sidebarRow__genre" to="/choose-music-binaural-beats-relax">Relax</Link>
-        <Link className="sidebarRow__genre" to="/choose-music-binaural-beats-focus">Focus</Link>
-        <Link className="sidebarRow__genre" to="/choose-music-binaural-beats-motivation">Motivation</Link>
+        <Link
+          className="sidebarRow__genre"
+          to="/choose-music-binaural-beats-relax"
+        >
+          Relax
+        </Link>
+        <Link
+          className="sidebarRow__genre"
+          to="/choose-music-binaural-beats-focus"
+        >
+          Focus
+        </Link>
+        <Link
+          className="sidebarRow__genre"
+          to="/choose-music-binaural-beats-motivation"
+        >
+          Motivation
+        </Link>
       </div>
 
       <div className="sidebarRow">
         <h2 className="sidebarRow__genre">{titleAndGenres.title.four}</h2>
-        <Link className="sidebarRow__genre" to="/choose-music-nature-sounds-rain">Rain</Link>
-        <Link className="sidebarRow__genre" to="/choose-music-nature-sounds-nature">Nature</Link>
-        <Link className="sidebarRow__genre" to="/choose-music-nature-sounds-fireplace">Fireplace</Link>
+        <Link
+          className="sidebarRow__genre"
+          to="/choose-music-nature-sounds-rain"
+        >
+          Rain
+        </Link>
+        <Link
+          className="sidebarRow__genre"
+          to="/choose-music-nature-sounds-nature"
+        >
+          Nature
+        </Link>
+        <Link
+          className="sidebarRow__genre"
+          to="/choose-music-nature-sounds-fireplace"
+        >
+          Fireplace
+        </Link>
+      </div>
+
+      <div className="pomo-container">
+        <button className="pomodoro" onClick={() => setTime === 5000}>Pomodoro</button>
+        <div className="pomo-break-timers">
+          <button>Short Break</button>
+          <button>Long Break</button>
+        </div>
+        <div className="timer">{time}</div>
+        <button className="start-button">Start</button>
       </div>
     </div>
   );
